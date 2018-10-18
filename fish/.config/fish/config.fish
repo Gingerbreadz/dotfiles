99b9bc344985a3723365938f499fc340d91aa5fe
Anatole Lefort <anatole.lefort@gmail.com>
2018-10-18 21:14:42 +0200

    fish: add vpn service alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,5 +9,6 @@ alias "wls1"="sudo rc-service net.wls1"
 alias "vpn1minet"="sudo rc-service openvpn.vpn1minet"
 alias "vpn2minet"="sudo rc-service openvpn.vpn2minet"
 alias "vpn4minet"="sudo rc-service openvpn.vpn4minet"
+alias "vpnimdea"="sudo rc-service openvpn.imdea"
 
 alias "mpv"="mpv --no-osc"
